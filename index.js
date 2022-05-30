e36f970976ff731adaf5564ca23696a6d75fc9c7
jakir10 <jakir.cse.bubt@gmail.com>
2022-05-30 23:45:38 +0600

    before deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+// middleware set
 app.use(cors());
 app.use(express.json());
 
